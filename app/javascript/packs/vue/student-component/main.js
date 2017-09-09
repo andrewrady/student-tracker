5dbea0888815ab91eab783800ceac6044a7fb520
Andrew Rady <andrewrady@Andrews-Air.hitronhub.home>
2017-09-09 01:17:21 -0600

    added store to student component

--- app/javascript/packs/vue/student-component/main.js
+++ app/javascript/packs/vue/student-component/main.js
@@ -1,20 +1,19 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import vueCustomElement from 'vue-custom-element';
-// import cloneDeep from 'lodash.clonedeep';
-// import { ContractingSvc } from './store/contracting-service';
+import cloneDeep from 'lodash.clonedeep';
 
-// function createVueStore(store){
-//     return new Vuex.Store(cloneDeep(store));
-// }
+function createVueStore(store){
+    return new Vuex.Store(cloneDeep(store));
+}
 Vue.use(vueCustomElement);
 
 import studentCmp from './StudentCmp.vue';
-// import contractingStore from './store/store';
+import studentStore from './store/store';
 
 Vue.customElement('student-cmp', {
     beforeCreate: function() {
-        // this.$store = createVueStore(contractingStore);
+        this.$store = createVueStore(studentStore);
     },
     render: h => h(studentCmp)
 });
---
+++ app/javascript/packs/vue/student-component/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+export const state = {
+    //states
+}
+
+export const mutation = {
+    //changes to state
+}
+
+export const actions = {
+    //api calls
+}

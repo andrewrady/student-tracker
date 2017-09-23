2a1c02ab82e8ed96b4539e4edb827d67c9b164a1
Andrew Rady <andrewrady@Andrews-Air.hitronhub.home>
2017-09-23 02:33:19 -0600

    moved components around

--- app/javascript/packs/vue/student-component/components/ListStudentCmp.vue
+++ app/javascript/packs/vue/student-component/components/ListStudentCmp.vue
@@ -1,61 +1,41 @@
 <template>
     <div>
-        <div v-if="students.length == 0">No students</div>
-        <div v-if="students.length > 0">
-            <div class="ui grid">
-                <div class="four wide column">
-                    <h4>Name</h4>
-                </div>
-                <div class="four wide column">
-                    <h4>Rank</h4>
-                </div>
-                <div class="two wide column">
-                    <h4>Size</h4>
-                </div>
-                <div class="two wide column">
-                    <h4>Age</h4>
-                </div>
-                <div class="three wide column">
-                    <h4>Program</h4>
-                </div>
-            </div>
-            <div class="student-list" v-for='(student, index) in students' :key='index'>
-                <div class="ui top attached header" @click="toggleContent">
-                    <div class="ui grid">
-                        <div class="four wide column">
-                            {{ student.name }}
-                        </div>
-                        <div class="four wide column">
-                            <p>{{ student.rank }}</p>
-                        </div>
-                        <div class="two wide column">
-                            <p>{{ student.size }}</p>
-                        </div>
-                        <div class="two wide column">
-                            <p>{{ student.age }}</p>
-                        </div>
-                        <div class="one wide column right">
-                            <p>K4K</p>
-                        </div>
+        <div class="student-list">
+            <div class="ui top attached header" @click="toggleContent">
+                <div class="ui grid">
+                    <div class="four wide column">
+                        {{ student.name }}
+                    </div>
+                    <div class="four wide column">
+                        <p>{{ student.rank }}</p>
+                    </div>
+                    <div class="two wide column">
+                        <p>{{ student.size }}</p>
+                    </div>
+                    <div class="two wide column">
+                        <p>{{ student.age }}</p>
+                    </div>
+                    <div class="one wide column right">
+                        <p>K4K</p>
                     </div>
                 </div>
-                <div v-if='content' class="ui attached segment">
-                    <div class="ui grid">
-                        <div class="six wide column">
-                            <address>Address: {{ student.address }}</address>
-                        </div>
-                        <div class="three wide column">
-                            <p>Size: {{ student.size }}</p>
-                        </div>
-                        <div class="four wide column">
-                            <p>Phone: {{ student.phone }}</p>
-                        </div>
-                        <div class="three wide column">
-                            <a href="http://localhost.com/students/1">
-                                <i class="edit icon"></i>
-                            </a>
-                            <i class="trash icon" @click="deleteStudent(student.id)"></i>
-                        </div>
+            </div>
+            <div v-if='content' class="ui attached segment">
+                <div class="ui grid">
+                    <div class="six wide column">
+                        <address>Address: {{ student.address }}</address>
+                    </div>
+                    <div class="three wide column">
+                        <p>Size: {{ student.size }}</p>
+                    </div>
+                    <div class="four wide column">
+                        <p>Phone: {{ student.phone }}</p>
+                    </div>
+                    <div class="three wide column">
+                        <a href="http://localhost.com/students/1">
+                            <i class="edit icon"></i>
+                        </a>
+                        <i class="trash icon" @click="deleteStudent(student.id)"></i>
                     </div>
                 </div>
             </div>
@@ -68,18 +48,11 @@ import { mapState } from 'vuex';
 
 export default {
     name: 'ActiveStudent',
+    props: ['student'],
     data() {
         return {
             content: false
         }
-    },
-    computed: {
-        ...mapState([
-            'students'
-        ])
-    },
-    mounted() {
-        this.$store.dispatch('populateStudents')
     },
     methods: {
         toggleContent() {
